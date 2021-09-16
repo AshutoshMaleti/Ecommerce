f882654e5f731264c59232dccb4807b3ef98212c
AshutoshMaleti <ashutosh.maleti@dianapps.com>
2021-09-16 18:12:10 +0530

    orders.views.py still has some unwritten funcitonalities

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-#from django.http.response import HttpResponse
+from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404
 
 from rest_framework.response import Response
@@ -8,7 +8,29 @@ from .models import *
 from .serializers import *
 
 # Create your views here.
-def AddToCart(request, pk):
+def AddToCart(request):
+    if request.user.is_authenticated:
+        print('inside is_authenticated')
+        user = request.user.id
+        print(user)
+        customerQs = Customers.objects.filter(user=user).first()
+        print(customerQs)
+        #print(customerQs[0]['id'])
+        orderQs = Orders.objects.get_or_create(customer=customerQs, status=False)
+        print(orderQs)
+        orderProduct=orderQs.OrderHasProduct_set.all()
+        print(orderProduct)
+
+        '''customer = request.user.customer
+        order = Orders.objects.get_or_create(customer)
+        product = order.orderhasproduct_set.all()'''
+    else:
+        items = []
+        order = {'items':items}
+
+    return Response('chal rha hai')
+
+'''def AddToCart(request, pk):
     productId = get_object_or_404(Products, id=pk)
     orderItem = OrderHasProduct.objects.filter(productId)
     orderQs = Orders.objects.filter(customerId=request.User, status=False)
@@ -19,7 +41,7 @@ def AddToCart(request, pk):
             orderQs.save()
         else:
             orderItem = OrderHasProduct.objects.create(order=productId)
-    #return HttpResponse('This is Add Items page.')
+    return HttpResponse('This is Add Items page.')'''
 
 @api_view(['POST'])
 def Order(request, pk):
