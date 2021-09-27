5e2ee00a9bab178aaee777cfdf4d00f7cffa6b1c
AshutoshMaleti <ashutosh.maleti@dianapps.com>
2021-09-27 18:28:40 +0530

    added @login_required decorator

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('signin', views.signin, name='signin'),
-    path('signup', views.signup, name='signup'),
-    path('signout',views.signout, name='signout'),
+    path('signin/', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
+    path('signout/',views.signout, name='signout'),
 ]
