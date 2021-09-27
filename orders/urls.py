c4ef440bc1fb6727ee3eba017a0e9ca39bc9dfcf
AshutoshMaleti <ashutosh.maleti@dianapps.com>
2021-09-27 15:50:29 +0530

    everything working

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,8 @@ from orders import views
 
 urlpatterns = [
     path('add-to-cart/<str:pk>/', views.AddToCart, name='AddToCart'),
+    path('remove-from-cart/<str:pk>/', views.RemoveFromCart, name='RemoveFromCart'),
+    path('delete-from-cart/<str:pk>/', views.DeleteItem, name='DeleteItem'),
+    path('order-summary/', views.OrderSummary, name='OrderSummary'),
     path('order/', views.Orders, name='Order')
 ]
